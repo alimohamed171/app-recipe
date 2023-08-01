84f652e305bdd29ce84ac169c08a8bd4fe3966ec
yn <alimohammed1712003@gmail.com>
2023-08-01 22:36:29 +0300

    register fragment

--- app/src/main/java/com/example/testrepo/LoginFragment.kt
+++ app/src/main/java/com/example/testrepo/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
 
 class LoginFragment : Fragment() {
 
@@ -15,6 +17,14 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.txtSignUP).setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_registerFragment)
+
+        }
     }
 
 }
