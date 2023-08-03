762063bfa00919ce617c7b5bd3c823b152a336b6
Sara <saraalmohnds0@gmail.com>
2023-08-03 08:58:33 +0300

    added favourite button in details fragment

--- app/src/main/java/com/example/testrepo/DetailFragment.kt
+++ app/src/main/java/com/example/testrepo/DetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import androidx.fragment.app.Fragment
 
 class DetailFragment : Fragment() {
@@ -16,4 +17,12 @@ class DetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    private fun onCheckedFavouriteButton() {
+//        checkBox.setOnCheckedChangeListener(checkBox, isChecked ->
+//        if (isChecked)
+//            TODO()
+//        else
+//            TODO()
+//        )
+    }
 }
